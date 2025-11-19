9369f6e9473d6776d1c0fc1ecd309c531366b1e6
jeanluc0099 <judeazanmadje@gmail.com>
2025-11-19 17:20:31 +0100

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-function nettoyerEvenements() {
-    let events = JSON.parse(localStorage.getItem('events')) || [];
-    const now = new Date();
-    events = events.filter(e => new Date(e.date) >= now);
-    localStorage.setItem('events', JSON.stringify(events));
-}
-nettoyerEvenements();

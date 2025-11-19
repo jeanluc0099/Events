67b49864b0f7a66806c8f0233ecf5b189c0a8309
jeanluc0099 <judeazanmadje@gmail.com>
2025-11-19 16:45:59 +0100

    Update contact.js

--- contact.js
+++ contact.js
@@ -1,13 +1,13 @@
-document.getElementById('contact-form').addEventListener('submit', (e) => {
+document.getElementById('contact-form').addEventListener('submit',(e)=>{
     e.preventDefault();
-    const messages = JSON.parse(localStorage.getItem('messages')) || [];
+    const messages=JSON.parse(localStorage.getItem('messages'))||[];
     messages.push({
-        name: document.getElementById('name').value,
-        email: document.getElementById('email').value,
-        message: document.getElementById('message').value,
-        date: new Date()
+        name:document.getElementById('name').value,
+        email:document.getElementById('email').value,
+        message:document.getElementById('message').value,
+        date:new Date()
     });
-    localStorage.setItem('messages', JSON.stringify(messages));
+    localStorage.setItem('messages',JSON.stringify(messages));
     alert('Message envoyé !');
     e.target.reset();
 });

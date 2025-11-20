e16c2da55b3aa30e95f4a545efeb56d8ddef294d
jeanluc0099 <judeazanmadje@gmail.com>
2025-11-20 13:37:16 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,41 @@
+// Import Firebase SDKs
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut
+} from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
+
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs
+} from "https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js";
+
+// Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "TON_API_KEY",
-  authDomain: "TON_DOMAINE.firebaseapp.com",
-  projectId: "TON_PROJECT_ID",
-  storageBucket: "TON_BUCKET.appspot.com",
-  messagingSenderId: "TON_ID",
-  appId: "TON_APP_ID"
+  apiKey: "AIzaSyAWX2UN5whFp1GXKhQpD6HFTZx2sfCSLfo",
+  authDomain: "events-app-754f4.firebaseapp.com",
+  projectId: "events-app-754f4",
+  storageBucket: "events-app-754f4.firebasestorage.app",
+  messagingSenderId: "908087963056",
+  appId: "1:908087963056:web:024a5fae9c2a1229397a75",
+  measurementId: "G-2NYNMFR16Q"
 };
 
-firebase.initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
-const auth = firebase.auth();
-const db = firebase.firestore();
+// EXPORTS
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export {
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+  collection,
+  addDoc,
+  getDocs
+};

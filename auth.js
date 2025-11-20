17113d71b4ebc74131fcf67ac52576d626c94495
jeanluc0099 <judeazanmadje@gmail.com>
2025-11-20 01:11:33 +0100

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,52 @@
+// INSCRIPTION
+if (document.getElementById("registerForm")) {
+  document.getElementById("registerForm").addEventListener("submit", e => {
+    e.preventDefault();
 
+    const email = document.getElementById("regEmail").value;
+    const pass = document.getElementById("regPassword").value;
+
+    auth.createUserWithEmailAndPassword(email, pass)
+      .then(() => {
+        alert("Compte créé !");
+        window.location.href = "promoteur.html";
+      })
+      .catch(err => alert(err.message));
+  });
+}
+
+// CONNEXION
+if (document.getElementById("loginForm")) {
+  document.getElementById("loginForm").addEventListener("submit", e => {
+    e.preventDefault();
+
+    const email = document.getElementById("email").value;
+    const pass = document.getElementById("password").value;
+
+    auth.signInWithEmailAndPassword(email, pass)
+      .then(() => {
+        window.location.href = "promoteur.html";
+      })
+      .catch(err => alert(err.message));
+  });
+}
+
+// PROTECTION DES PAGES
+auth.onAuthStateChanged(user => {
+  const protectedPages = ["promoteur.html"];
+
+  if (protectedPages.includes(location.pathname.replace("/", ""))) {
+    if (!user) {
+      window.location.href = "login.html";
+    }
+  }
+});
+
+// DÉCONNEXION
+if (document.getElementById("logoutBtn")) {
+  document.getElementById("logoutBtn").onclick = () => {
+    auth.signOut().then(() => {
+      window.location.href = "login.html";
+    });
+  };
+}

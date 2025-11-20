82f91100d872db3d1f362fbed1cffe0ef5c40df5
jeanluc0099 <judeazanmadje@gmail.com>
2025-11-20 01:10:52 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,13 @@
+const firebaseConfig = {
+  apiKey: "TON_API_KEY",
+  authDomain: "TON_DOMAINE.firebaseapp.com",
+  projectId: "TON_PROJECT_ID",
+  storageBucket: "TON_BUCKET.appspot.com",
+  messagingSenderId: "TON_ID",
+  appId: "TON_APP_ID"
+};
 
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.firestore();
